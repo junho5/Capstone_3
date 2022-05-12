5b595a9a84194190873f0d3cf9c99b19c8b2728d
Oh JUN Ho <dhwnsghdhkd@naver.com>
2022-05-12 14:00:45 +0900

    image test finish

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const path = require('path');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+module.exports = s3;
 
 var db_config = require(__dirname + '/config/database.js');
 var conn = db_config.init();
