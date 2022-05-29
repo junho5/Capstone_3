ed5c9479beb85d539eeceefa7a214baa4f8e684d
Oh JUN Ho <dhwnsghdhkd@naver.com>
2022-05-30 00:56:10 +0900

    merge

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const AWS = require('aws-sdk');
 const fs = require('fs');
 const dotenv = require('dotenv');
+const PythonShell = require('python-shell');
 dotenv.config();
 
 // aws
@@ -37,21 +38,21 @@ app.use(express.static(path.join(__dirname, 'routes')));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use(
-    session({
-        resave: false,
-        saveUninitialized: false,
-        secret: process.env.COOKIE_SECRET,
-        cookie: {
-            httpOnly: true,
-            secure: false,
-            maxAge: 600000,
-        },
-        name: 'master_check-session-cookie',
-    })
-);
+// app.use(
+//     session({
+//         resave: false,
+//         saveUninitialized: false,
+//         secret: process.env.COOKIE_SECRET,
+//         cookie: {
+//             httpOnly: true,
+//             secure: false,
+//             maxAge: 600000,
+//         },
+//         name: 'master_check-session-cookie',
+//     })
+// );
 
 // 요청 경로에 따라 router 실행
 app.use('/',loginRouter);
@@ -74,15 +75,122 @@ app.get('/recommend',(req,res)=>{
 app.get('/recommend/input',(req,res)=>{
     res.render('input');
 });
-app.post('/recommend/input',(req,res)=>{
+app.get('/recommend/output',(req,res)=>{
     console.log(req.body);
-    res.send(req.body);
-    res.end();
+    var inputQuery = req.query
+    var sql = "SELECT * FROM plant"
+    conn.query(sql, function (err, inputData) {
+        if (err) {
+            console.log(err +"mysql 조회 실패");
+            return
+        }else{
+            var options = {
+                mode: 'text',
+                pythonPath: '',
+                pythonOptions: ['-u'],
+                scriptPath: 'public/',
+                args: ['value1', JSON.stringify({inputQuery}), JSON.stringify({inputData})]
+              };
+            //   console.log(inputData)
+            PythonShell.PythonShell.run('test.py', options, function (err, results) {
+                if (err) {
+                    console.log(err);
+                }else {
+                    res.render('output',{
+                        Data: JSON.parse(results)});
+                    console.log(JSON.parse(results));
+                }
+          });
+
+        }
+    });    
+});
+
+app.post('/recommend/output',(req,res)=>{
+    // backURL=req.header()
+    console.log(req.body);
+    //평가점수 db 처리 할 부분.
+    res.send(`<script type="text/javascript">alert("평가 완료!"); window.location = document.referrer;; </script>`);
+    
 });
 
 app.get('/search',(req,res)=>{
     res.redirect('/pasing/' + 1)
+    
+    // res.redirect('/search/1');
 });
+app.get('/pasing/:cur',(req,res)=>{
+    var page_size = 15;
+    var page_list_size =10;
+    var no = "";
+    var totalPageCount =0;
+    var sql = 'SELECT count(*) as cnt FROM plant';   
+    conn.query(sql, function (err, data) {
+        if (err) {
+            console.log(err +"mysql 조회 실패");
+            return
+        }
+        totalPageCount = data[0].cnt
+
+        var curPage=req.params.cur;
+        console.log("현재페이지: " +curPage,"전체 페이지:" +totalPageCount);
+        if (totalPageCount<0){
+            totalPageCount =0
+        }
+       
+        var totalPage = Math.ceil(totalPageCount/page_size);
+        var totalSet = Math.ceil(totalPage/ page_list_size);
+        var curSet = Math.ceil(curPage / page_list_size);
+        var startPage = ((curSet-1)*10)+1;
+        var endPage = (startPage+page_list_size)-1;
+
+        if (curPage<0){
+            no =0
+        } else{
+            no = (curPage-1) *15
+        }
+        console.log('[0] curPage : ' + curPage + ' | [1] page_list_size : ' + page_list_size + ' | [2] page_size : ' + page_size + ' | [3] totalPage : ' + totalPage + ' | [4] totalSet : ' + totalSet + ' | [5] curSet : ' + curSet + ' | [6] startPage : ' + startPage + ' | [7] endPage : ' + endPage)
+        var result2 = {
+            "curPage": curPage,
+            "page_list_size": page_list_size,
+            "page_size": page_size,
+            "totalPage": totalPage,
+            "totalSet": totalSet,
+            "curSet": curSet,
+            "startPage": startPage,
+            "endPage": endPage
+        };
+        console.log("몇번부터 몇번까지냐~~~~~~~" + no);
+
+        var queryString = 'select * from plant order by plant_num limit ?,?';
+            conn.query(queryString, [no, page_size], function (error, result) {
+                if (error) {
+                    console.log("페이징 에러" + error);
+                    return
+                }
+                console.log(no,page_size);
+                console.log(result2);
+                console.log(result);
+                res.render('search',{
+                    data: result,
+                    pasing: result2
+                });
+        });
+    });
+});
+
+app.get('/detail/:id',(req,res)=>{
+    console.log(req.params.id);
+    var queryString = 'select * from plant where plant_num = ?';
+    conn.query(queryString,[req.params.id],(error,result)=>{
+        console.log(result);
+        res.render('detail',{
+            data:result[0]
+        });
+    });
+
+});
+
 
 // 404 에러처리 미들웨어 (사용자 요청이라서 500위에 작성)
 app.use((req, res, next) => {
