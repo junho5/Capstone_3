7a7a21f0a56405242d5f18c08c5a569c740919d8
Oh JUN Ho <dhwnsghdhkd@naver.com>
2022-06-06 17:52:22 +0900

    upload finish; git push

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const dotenv = require('dotenv');
 const PythonShell = require('python-shell');
+const multer = require('multer')
+const multerS3 = require('multer-s3');
 dotenv.config();
 
 // aws
@@ -18,6 +20,28 @@ const s3 = new AWS.S3({
 });
 module.exports = s3;
 
+// upload
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'plant-imageset',
+      acl: 'public-read-write',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      key: function (req, file, cb) {
+        var sql = "select AUTO_INCREMENT as id_num from information_schema.tables where table_name = 'plant' AND table_schema = DATABASE()"
+        conn.query(sql,function(err,data){
+            if (err){
+                console.log("실패")
+              }else{
+                const ext = path.extname(file.originalname);//업로드 파일이름을 원본 파일이름 +시간+확장자로 저장
+                console.log(data[0].id_num);
+                cb(null, data[0].id_num+ext);
+        }
+        }
+    )},
+    }),
+  });
+
 // db
 var db_config = require(__dirname + '/config/database.js');
 var conn = db_config.init();
@@ -25,6 +49,7 @@ db_config.connect(conn);
 
 // import routers
 const loginRouter = require('./routes/index');
+const { text } = require('express');
 
 
 const app = express();
@@ -170,6 +195,56 @@ app.get('/detail/:id',(req,res)=>{
 
 });
 // comment 부분 추가 예정
+
+
+//--------------------------------------------------------
+app.get('/admin', (req,res)=>{
+    var sql = "select AUTO_INCREMENT as id_num from information_schema.tables where table_name = 'plant' AND table_schema = DATABASE()"
+    conn.query(sql, function(err, id_num){
+        if (err) {
+            console.log(err +"mysql 조회 실패");
+            return
+        }else{
+            var sql = "select * from plant"
+            conn.query(sql, function(err, data){
+                if (err) {
+                    console.log('mysql 조회 실패');
+
+                }else{
+                    res.render('admin',{id_num: id_num, data: data});
+                    // console.log(data);
+
+                }
+            })
+
+        }
+    });
+});
+app.post('/admin',upload.single('file'),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file,req.body);
+    var sql = "INSERT INTO plant(plant_num,cluster_num,plant_name,light,temp,water,height,width,poison,image,comment) VALUES(null,1,?,?,?,?,?,?,?,?,'comment')"
+    conn.query(sql,[req.body.name,parseInt(req.body.light),parseInt(req.body.temp),parseInt(req.body.water),parseInt(req.body.height),parseInt(req.body.width), parseInt(req.body.poison),req.body.image],function(err,rows){
+      if(err){
+        console.log("upload 실패");
+      } else{
+        console.log(rows.insertId);
+        res.redirect('/upload');
+
+      }
+      
+    })
+})
+
+// app.post('/admin', upload.single('image'));
+
+
+// app.post('/admin', (req,res)=>{
+//     res.send(req.body);
+//     console.log(req.body);
+//     // res.redirect('/admin')
+// });
+//--------------------------------------------------------
 
 // 404 에러처리 미들웨어 (사용자 요청이라서 500위에 작성)
 app.use((req, res, next) => {
